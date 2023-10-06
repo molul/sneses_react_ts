af5a863d05e83b514d2294cc56a76c2ee034e310
molul <luismorcillo@gmail.com>
2023-10-06 18:36:08 +0200

    Tutorials finished

--- src/assets/data/tutorials.ts
+++ src/assets/data/tutorials.ts
@@ -5,37 +5,7 @@ export const tutorials = [
       {
         type: "pdf",
         text: "Official quick start guide",
-        url: "/doc/Blast16_-_Quick_start_guide.pdf",
-      },
-      {
-        type: "video",
-        text: "Installation guide by Madlittlepixel",
-        url: "https://www.youtube.com/watch?v=04Rq8aoJzto",
-      },
-      {
-        type: "video",
-        text: "Installation guide by ETA Prime",
-        url: "https://www.youtube.com/watch?v=YkLBb1LThwk",
-      },
-      {
-        type: "video",
-        text: "Installation guide by Restopia",
-        url: "https://www.youtube.com/watch?v=C0QGQ2RqGWU",
-      },
-      {
-        type: "video",
-        text: "Installation tutorial by Patton Plays",
-        url: "https://www.youtube.com/watch?v=CRfXwcNkR7A",
-      },
-      {
-        type: "video",
-        text: "Installation guide by Sega Lord X",
-        url: "https://www.youtube.com/watch?v=TeurJjk7P9o",
-      },
-      {
-        type: "post",
-        text: "Convert Mega CD/Sega CD games to chd",
-        url: "https://www.reddit.com/r/RetroPie/comments/72kh6q/stepbystep_guide_how_to_convert_sega_cd_or_pc/",
+        url: "/doc/SNESES_-_Quick_start_guide.pdf",
       },
     ],
   },
@@ -45,22 +15,7 @@ export const tutorials = [
       {
         type: "pdf",
         text: "Guía de inicio rápido oficial",
-        url: "/doc/Blast16_-_Gu%c3%ada_de_inicio_r%c3%a1pido.pdf",
-      },
-      {
-        type: "video",
-        text: "Tutorial completo por TuberViejuner",
-        url: "https://www.youtube.com/watch?v=5jcqukpl0Vg",
-      },
-    ],
-  },
-  {
-    language: "Portuguese",
-    data: [
-      {
-        type: "post",
-        text: "Installation guide by Rafael Rigues",
-        url: "http://rigues.badcoffee.info/2019/06/11/como-montar-seu-mega-drive-mini/",
+        url: "/doc/SNESES_-_Guia_de_inicio_rapido.pdf",
       },
     ],
   },
