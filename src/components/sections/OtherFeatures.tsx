6ab272598622aa94a846938c1818e2418256b762
molul <luismorcillo@gmail.com>
2023-10-09 20:12:26 +0200

    Actualizar OtherFeatures.tsx

--- src/components/sections/OtherFeatures.tsx
+++ src/components/sections/OtherFeatures.tsx
@@ -1,5 +1,6 @@
 import { features } from "../../assets/data/features";
 import Title from "../Title";
+import { AiFillCheckSquare } from "react-icons/ai";
 
 const OtherFeatures = () => {
   return (
@@ -8,8 +9,13 @@ const OtherFeatures = () => {
       <div className=" break-words bg-black bg-opacity-50 p-4 rounded-lg">
         <div className="space-y-2">
           {features.other.map((feat, index) => (
-            <div key={index} className="py-2">
-              {feat}
+                   <div key={index} className="flex  justify-start items-start gap-3 py-2">
+  <div className="pt-0.5">
+<AiFillCheckSquare/>
+</div>
+<div>
+{feat}
+</div>
             </div>
           ))}
         </div>
