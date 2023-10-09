aed41fbac4f598b1fbc746e3420f59cadff83fe8
molul <luismorcillo@gmail.com>
2023-10-09 20:10:20 +0200

    Actualizar KeyFeatures.tsx

--- src/components/sections/KeyFeatures.tsx
+++ src/components/sections/KeyFeatures.tsx
@@ -12,7 +12,7 @@ const KeyFeatures = () => {
         <div className="space-y-2">
           {features.key.map((feat, index) => (
             <div key={index} className="flex  justify-start items-start gap-3 py-2">
-  <div className="pt-1">
+  <div className="pt-0.5">
 <AiFillCheckSquare/>
 </div>
 <div>
