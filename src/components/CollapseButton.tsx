21bdd2f4a271eb90fe059b1c5b2ddbf81ab44037
molul <luismorcillo@gmail.com>
2023-10-11 08:12:47 +0200

    FAQ items were not displaying properly on mobile

--- src/components/CollapseButton.tsx
+++ src/components/CollapseButton.tsx
@@ -24,12 +24,16 @@ const CollapseButton = ({ text, expanded, toggleFunc }: Props) => {
         onClick={() => toggleFunc()}
       >
         <div className="flex items-center gap-2">
-          <AiFillCaretDown
-            className={`transition-transform ${
-              expanded ? "rotate-0" : "-rotate-90"
-            }`}
-          />
-          {text}
+          <div className="flex items-top gap-2">
+            <div className="pt-0.5">
+              <AiFillCaretDown
+                className={`transition-transform ${
+                  expanded ? "rotate-0" : "-rotate-90"
+                }`}
+              />
+            </div>
+            <div>{text}</div>
+          </div>
         </div>
       </button>
     </div>
