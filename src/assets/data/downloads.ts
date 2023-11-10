e422e8b87d0f39c2d6d1cdd21ff6b5f4fd14e5bd
molul <luismorcillo@gmail.com>
2023-11-10 22:52:22 +0100

    Downloads now go to https://tripware.es

--- src/assets/data/downloads.ts
+++ src/assets/data/downloads.ts
@@ -1,4 +1,4 @@
-const basePath = "http://tripware.es/wp-content/uploads/";
+const basePath = "https://tripware.es/wp-content/uploads/";
 const snesesPath = basePath + "sneses/";
 
 export const downloads = [
